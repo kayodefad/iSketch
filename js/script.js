dd8ec3b4221809aedd3c11a5da432663708d0a15
Oluwakayode <fadojuk@gmail.com>
2020-07-17 12:04:48 +0100

    final one

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const subscribeButton = document.querySelector('.subscribe-button')
+// const subscribeButton = document.querySelector('.subscribe-button')
 const showSubscribe = document.querySelector('.show-subscribe')
 
-subscribeButton.addEventListener('click', () => {
-  showSubscribe.innerHTML = `
-    <form class="submit-email">
-    <div class="mr-1">
-      <input type="email" name="email" placeholder="Enter email" required>
-    </div>
-    <button class="subscribe-success" type="submit">Submit</button>
-    </form>
-  `
-})
+// subscribeButton.addEventListener('click', () => {
+//   showSubscribe.innerHTML = `
+//     <form class="submit-email">
+//     <div class="mr-1">
+//       <input type="email" name="email" placeholder="Enter email" required>
+//     </div>
+//     <button class="subscribe-success" type="submit">Submit</button>
+//     </form>
+//   `
+// })
 
-document.querySelector('.subscribe-success').addEventListener('click', () => {
-  window.location.replace = 'index.html'
-})
+// const subscribeSuccess = document.querySelector('.subscribe-success')
+  
+// subscribeButton.addEventListener('click', () => {
+//   window.location.replace = 'index.html'
+// })
